767d6b3e0f467adce0bb96931e7cece86fa4fec6
Vishwajeet Kumar <vishwajeetkmr7@gmail.com>
2018-04-29 03:02:56 +0530

    added pages and menu functionality

--- footer.php
+++ footer.php
@@ -12,9 +12,14 @@
         <div class="site-footer__col-two-three-group">
           <div class="site-footer__col-two">
             <h3 class="headline headline--small">Explore</h3>
-            <nav>
+            <nav class="nav-list">
+                
+            <!--<?php wp_nav_menu(array(
+                'theme_location' => 'footerLocationOne'
+            )); ?>-->
+                
               <ul class="nav-list min-list">
-                <li><a href="#">About Us</a></li>
+                <li><a href="<?php echo site_url('about-us'); ?>">About Us</a></li>
                 <li><a href="#">Programs</a></li>
                 <li><a href="#">Events</a></li>
                 <li><a href="#">Campuses</a></li>
@@ -24,10 +29,15 @@
 
           <div class="site-footer__col-three">
             <h3 class="headline headline--small">Learn</h3>
-            <nav>
+            <nav class="nav-list">
+                
+            <!--<?php wp_nav_menu(array(
+                'theme_location' => 'footerLocationTwo'
+            )); ?>-->    
+                
               <ul class="nav-list min-list">
                 <li><a href="#">Legal</a></li>
-                <li><a href="#">Privacy</a></li>
+                <li><a href="<?php echo site_url('privacy-policy'); ?>">Privacy</a></li>
                 <li><a href="#">Careers</a></li>
               </ul>
             </nav>
